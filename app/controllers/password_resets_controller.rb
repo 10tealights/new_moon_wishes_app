565fee235ba90e02c3481174d43fa9b62d0f41ed
shoko <99260932+10tealights@users.noreply.github.com>
2022-09-02 08:34:34 +0900

    Add: パスワード再設定機能の追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,27 @@ class PasswordResetsController < ApplicationController
     redirect_to root_path, notice: t('.reset_success')
   end
 
-  def edit; end
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
 
-  def update; end
+    not_authenticated if @user.blank?
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    not_authenticated if @user.blank?
+
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      redirect_to login_path, notice: t('.change_success')
+    else
+      flash[:alert] = t('.change_failed')
+      render :edit
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
 end
