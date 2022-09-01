a290e1e15d6d05abc3cf6ea7523764746697502b
shoko <99260932+10tealights@users.noreply.github.com>
2022-09-02 06:32:39 +0900

    Add: メールアドレスでのログイン機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :account_id, :password, :password_confirmation)
+    params.require(:user).permit(:name, :account_id, :email, :password, :password_confirmation)
   end
 end
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -247,7 +247,7 @@ Rails.application.config.sorcery.configure do |config|
     # Specify username attributes, for example: [:username, :email].
     # Default: `[:email]`
     #
-    user.username_attribute_names = :account_id
+    user.username_attribute_names = %i[account_id email]
 
     # Change *virtual* password attribute, the one which is used until an encrypted one is generated.
     # Default: `:password`
