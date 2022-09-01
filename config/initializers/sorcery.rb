f48d7a0af69b8a3267207e5c5359c824c6b0a2cb
shoko <99260932+10tealights@users.noreply.github.com>
2022-09-02 08:36:59 +0900

    Add: パスワードリセットURLの有効期間を1日に設定

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -419,7 +419,7 @@ Rails.application.config.sorcery.configure do |config|
     # How many seconds before the reset request expires. nil for never expires.
     # Default: `nil`
     #
-    # user.reset_password_expiration_period =
+    user.reset_password_expiration_period = 1.day
 
     # Hammering protection: how long in seconds to wait before allowing another email to be sent.
     # Default: `5 * 60`
