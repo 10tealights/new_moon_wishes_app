2fd3b6db6ee5e900d3dd093bb4b350d378c087bc
shoko <99260932+10tealights@users.noreply.github.com>
2022-09-02 04:25:42 +0900

    Add: ユーザー新規作成完了時に自動ログインする機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path, notice: t('defaults.message.created', item: User.model_name.human)
+      auto_login(@user)
+      redirect_to root_path, notice: t('defaults.message.created', item: User.model_name.human)
     else
       flash[:alert] = t('defaults.message.not_created', item: User.model_name.human)
       render :new
