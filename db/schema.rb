559c1f13dba24b0f34b1b6f6e972eb3a651b648e
shoko <99260932+10tealights@users.noreply.github.com>
2022-09-05 21:45:06 +0900

    Add: Wishesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_092050) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_123703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -50,6 +50,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_092050) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
+  create_table "wishes", force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.bigint "moon_id", null: false
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["moon_id"], name: "index_wishes_on_moon_id"
+    t.index ["user_id"], name: "index_wishes_on_user_id"
+  end
+
   create_table "zodiac_signs", force: :cascade do |t|
     t.integer "name", null: false
     t.datetime "created_at", null: false
@@ -58,4 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_092050) do
   end
 
   add_foreign_key "moons", "zodiac_signs"
+  add_foreign_key "wishes", "moons"
+  add_foreign_key "wishes", "users"
 end
