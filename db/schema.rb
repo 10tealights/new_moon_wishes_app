0c4866045d8cf0cffdb0eea14518f36b18f51c54
shoko <99260932+10tealights@users.noreply.github.com>
2022-09-14 22:07:37 +0900

    Add: Tagsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_145417) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_130312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_145417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["zodiac_sign_id"], name: "index_moons_on_zodiac_sign_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
