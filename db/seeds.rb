4c4b0857a0057f2f37591adbeedea426dc079b8f
shoko <99260932+10tealights@users.noreply.github.com>
2022-09-15 14:04:07 +0900

    Add: Tagsテーブル用seedデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,22 @@ CSV.foreach('db/csv/moons.csv', headers: true) do |row|
     fullmoon_time: row['fullmoon_time']
   )
 end
+
+Tag.create!(
+  [
+    { id: 1, name: '人生' },
+    { id: 2, name: '仕事' },
+    { id: 3, name: '人間関係' },
+    { id: 4, name: '勉強' },
+    { id: 5, name: '恋愛' },
+    { id: 6, name: 'パートナーシップ' },
+    { id: 7, name: 'セクシャリティ' },
+    { id: 8, name: '家庭' },
+    { id: 9, name: '生活' },
+    { id: 10, name: '健康' },
+    { id: 11, name: 'お金' },
+    { id: 12, name: '趣味' },
+    { id: 13, name: 'ダイエット' },
+    { id: 14, name: 'マインド' }
+  ]
+)
