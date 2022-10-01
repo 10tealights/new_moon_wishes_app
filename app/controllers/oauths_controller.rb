919d41ccebfdf4c6dd5e7111444bf2efc6f82141
shoko <99260932+10tealights@users.noreply.github.com>
2022-10-01 16:05:31 +0900

    Add: 既存ユーザーのLINEログイン機能の追加

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -5,16 +5,21 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
+    if auth_params[:error].present?
+      redirect_to root_path, alert: 'ログイン(連携)をキャンセルしました' 
+      return
+    end
+
     if @user = login_from(provider)
-      redirect_to root_path, notice:(t'.login_success', item: provider.titleize)
+      redirect_to root_path, notice:(t'.login_success', item: provider.upcase)
     else
       begin
-        @user = create_from(provider)
+        @user = current_user.present? ? update_from(provider) : create_from(provider)
         reset_session
         auto_login(@user)
-        redirect_to root_path, notice: (t'.login_success', item: provider.titleize)
+        redirect_to root_path, notice: (t'.login_success', item: provider.upcase)
       rescue
-        redirect_to root_path, alert: (t'.login_failed', item: provider.titleize)
+        redirect_to root_path, alert: (t'.login_failed', item: provider.upcase)
       end
     end
   end
@@ -28,5 +33,14 @@ class OauthsController < ApplicationController
   # Rails7で追加されたOpen Redirect protectionを無効化するためSorceryのメソッドをオーバーライド
   def login_at(provider_name, args = {})
     redirect_to sorcery_login_url(provider_name, args), allow_other_host: true
+  end
+
+  # すでにログイン中ユーザーがいる場合は既存のデータにLINE情報を紐づけ、UserモデルにLINEの情報を追加更新する
+  def update_from(provider_name, &block)
+    sorcery_fetch_user_hash provider_name
+    attrs = user_attrs(@provider.user_info_mapping, @user_hash)
+    current_user.update(attrs.except(:name))
+    current_user.add_provider_to_user(provider_name.to_s, @user_hash[:uid].to_s)
+    current_user
   end
 end
