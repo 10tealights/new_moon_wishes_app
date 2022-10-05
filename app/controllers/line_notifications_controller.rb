4c79e4540cbbe2a0c5635508b5676f1d8d0d976a
shoko <99260932+10tealights@users.noreply.github.com>
2022-10-05 10:29:49 +0900

    Add: 新月および満月のLINE通知ON/OFF機能の追加

---
+++ app/controllers/line_notifications_controller.rb
@@ -0,0 +1,13 @@
+class LineNotificationsController < ApplicationController
+  def update
+    current_user.change_notification_status(notification_params)
+    redirect_to profile_path, notice: t('.updated')
+  end
+
+  private
+
+  def notification_params
+    params.permit(:need_newmoon_msg, :need_fullmoon_msg)
+  end
+end
+
