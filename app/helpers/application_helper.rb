d905c20026c08c1f45ad6ccbb8bbf4be849f3bb5
shoko <99260932+10tealights@users.noreply.github.com>
2022-10-21 07:54:26 +0900

    Improve: 願いごと編集画面にCSSを適用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def hidden_if(action)
+    return 'hidden' if params[:action] == action
+  end
 end
