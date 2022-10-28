47a7a874c2f4f13d0b71305ef2f0e58b0e6b7e42
shoko <99260932+10tealights@users.noreply.github.com>
2022-10-28 23:04:57 +0900

    Add: Tailwindの設定ファイルにDaisyUIのオリジナルテーマ(配色)を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-  plugins: [require("daisyui")],
-  content: [
+    daisyui: {
+          themes: [
+            {
+              mytheme: {              
+                 "primary": "#3730a3",
+                 "secondary": "#4338ca",
+                 "accent": "#fef08a",
+                 "neutral": "#3730a3",
+                 "base-100": "#f8f8ff",
+                 "info": "#06b6d4",
+                 "success": "#16a34a",
+                 "warning": "#fcd34d",
+                 "error": "#e880b4",
+              },
+            },
+          ],
+    },
+    plugins: [require("daisyui")],
+    content: [
     './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
