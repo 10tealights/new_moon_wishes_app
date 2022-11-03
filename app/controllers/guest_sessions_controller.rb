5f17cf897e17d55741c4950fe966aa87aaa7d51e
shoko <99260932+10tealights@users.noreply.github.com>
2022-11-03 15:40:13 +0900

    Fix: 「今から言葉にしてみる」ボタン押下時にログイン中の場合は願いごと宣言画面に遷移するよう修正

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -2,9 +2,12 @@ class GuestSessionsController < ApplicationController
   skip_before_action :require_login
 
   def create
-    @guest_user = User.guest_generate
-    auto_login(@guest_user)
-
-    redirect_to new_wish_path, notice: t('.created')
+    if current_user
+      redirect_to new_wish_path
+    else
+      @guest_user = User.guest_generate
+      auto_login(@guest_user)
+      redirect_to new_wish_path, notice: t('.created')
+    end
   end
 end
