4357b58fd7dfd0c14da5f1f98bc3920a7f0f7708
shoko <99260932+10tealights@users.noreply.github.com>
2022-11-03 13:20:14 +0900

    Fix: パスワード再設定メール送信処理後の遷移先をログイン状態に応じて変更するよう修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,7 +2,11 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     @user&.deliver_reset_password_instructions!
-    redirect_to root_path, notice: t('.reset_success')
+    if current_user
+      redirect_to profile_path, notice: t('.reset_success')
+    else
+      redirect_to login_path, notice: t('.reset_success')
+    end
   end
 
   def edit
