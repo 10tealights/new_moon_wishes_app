0afd521955567ce28839589005c63fb568a06582
shoko <99260932+10tealights@users.noreply.github.com>
2022-12-01 16:48:14 +0900

    Add: APIを使用して月齢を取得し、月相画像を動的化して表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  require 'net/http'
+  require 'uri'
+  require 'json'
+
   def page_title(page_title = '')
     base_title = t 'defaults.site_title'
 
@@ -37,6 +41,38 @@ module ApplicationHelper
 
   def next_newmoon
     Moon.latest
+  end
+
+  def get_moon_age
+    today = Time.current
+    age_info = Net::HTTP.get(URI.parse("https://labs.bitmeister.jp/ohakon/json/?mode=moon_age&year=#{today.year}&month=#{today.month}&day=#{today.day}")) 
+    parsed_age_info = JSON.parse(age_info)
+    parsed_age_info['moon_age']
+  end
+
+  def moon_phase_image
+    moon_age = get_moon_age
+    newmoon = Moon.find_by(newmoon_time: Time.current.all_day)
+    fullmoon = Moon.find_by(fullmoon_time: Time.current.all_day)
+    moon_age_integer = moon_age.floor
+    difference = moon_age - moon_age_integer
+
+    case
+    when newmoon.present?
+      return 0
+    when fullmoon.present?
+      return 15
+    when moon_age_integer == 29
+      return 0
+    when difference >= 0.5
+      if moon_age_integer < 28
+        return moon_age_integer + 1
+      else
+        return 0 
+      end
+    when difference < 0.5
+      return moon_age_integer
+    end
   end
 
   def hidden_if(action)
