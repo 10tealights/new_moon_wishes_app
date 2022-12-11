c870e220c13b7e41e561188b3d894e5e0c7f1ca1
shoko <99260932+10tealights@users.noreply.github.com>
2022-12-11 23:14:34 +0900

    Add: 管理画面におけるログイン画面およびログイン機能の追加

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -5,8 +5,19 @@ class Admin::UserSessionsController < Admin::BaseController
 
   def new; end
 
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_to admin_root_path, notice: t('.login_success')
+    else
+      flash.now[:alert] = t('.login_failed')
+      render :new
+    end
+  end
+
   def destroy
     logout
-    redirect_to login_path, notice: t('.logout_success')
+    redirect_to admin_login_path, notice: t('.logout_success')
   end
 end
